7f4635df7248fec70979925e09c192ee6b796348
SA DAM <dams@dolphinsolutions.vn>
2023-12-29 20:34:50 +0700

    add footer

--- fe-FreshFood/src/layouts/DefaultLayout.jsx
+++ fe-FreshFood/src/layouts/DefaultLayout.jsx
@@ -1,10 +1,14 @@
 import React, { useState, useEffect } from 'react';
 import Header from './components/Header';
+import MyFooter from '../components/MyFooter';
+
 function DefaultLayout({ children }) {
     return (
         <div className='h-full overflow-hidden'>
             <Header />
             <div className='w-full h-auto overflow-x-hidden overflow-y-auto'>{children}</div>
+            <div className='h-10'></div>
+            <MyFooter />
         </div>
     );
 }
