25d4c210874fbff2d0b62c5bfd5860bb7505a33f
Đam Sa <20521154@ms.uit.edu.vn>
2023-12-29 10:11:21 +0700

    push from laptop

--- fe-FreshFood/src/pages/Home/index.jsx
+++ fe-FreshFood/src/pages/Home/index.jsx
@@ -2,41 +2,48 @@ import Sidebar from '../../layouts/components/Sidebar';
 import MyCarousel from '../../components/Carousel';
 function Home() {
     return (
-        <div className='flex flex-col w-full h-full px-[8vw] overflow-scroll items-center justify-start'>
+        <div className="flex flex-col w-full h-auto px-[8vw] overflow-scroll items-center justify-start">
             <Sidebar></Sidebar>
             {/* Khu hình ảnh ban đầu */}
-            <div className='grid grid-cols-3 h-[35vw] w-full  gap-8  items-center justify-between'>
+            <div className="grid grid-cols-1 lg:grid-cols-3 h-[35vw] min-h-fit w-full  gap-8  items-center justify-between">
                 {/* Hình ảnh lớn thứ nhất */}
-                <div className='flex relative col-span-2 flex-col justify-center items-start px-14 h-full w-full'>
-                    <img src='/1.jpg' alt='Banner1' className='absolute -z-0 left-0 top-0 w-full h-full' />
-                    <div className='z-10 flex flex-col gap-3 w-[60%]'>
-                        <div className='z-10 flex gap-3 text-sm '>
-                            <h6 className='text-[#4a5568]'>Exclusive offer</h6>
-                            <span className=' cursor-hover rounded-full px-2 text-[#ff4f4f] font-medium bg-gradient-to-r from-[rgba(255,114,114,0.15)] via-transparent to-transparent '>
+                <div className="flex relative col-span-2 flex-col justify-center items-start px-[2vw] h-full w-full">
+                    <img src="/1.jpg" alt="Banner1" className="absolute -z-0 left-0 top-0 w-full h-full" />
+                    <div className="z-10 flex flex-col gap-3 w-[60%]">
+                        <div className="z-10 flex gap-3 text-sm ">
+                            <h6 className="text-[#4a5568]">Exclusive offer</h6>
+                            <span className=" cursor-hover rounded-full px-2 text-[#ff4f4f] font-medium bg-gradient-to-r from-[rgba(255,114,114,0.15)] via-transparent to-transparent ">
                                 30% Off
                             </span>
                         </div>
                         <div>
-                            <h1 className='uppercase text-[#4a5568] text-[42px] font-semibold cursor-hover'>
-                                Stay home &amp; delivered your <span className='cursor-hover text-[#0da487] font-extrabold'>Daily Needs</span>
+                            <h1 className="uppercase text-[#4a5568] text-[42px] font-semibold cursor-hover">
+                                Stay home &amp; delivered your{' '}
+                                <span className="cursor-hover text-[#0da487] font-extrabold">Daily Needs</span>
                             </h1>
                         </div>
                         <div>
-                            <p className='w-[75%]'>Vegetables contain many vitamins and minerals that are good for your health.</p>
+                            <p className="w-[75%] text-[#4a5568] text-sm font-normal">
+                                Vegetables contain many vitamins and minerals that are good for your health.
+                            </p>
                         </div>
                         <div>
-                            <button className='text-red hover:before:bg-redborder-red-500 relative overflow-hidden bord er border-red-500 rounded-md bg-white px-6 py-4 text-red-500 shadow-2xl transition-all before:absolute before:bottom-0 before:left-0 before:top-0 before:z-0 before:h-full before:w-0 before:bg-red-500 before:transition-all before:duration-500 hover:text-white hover:shadow-red-500 hover:before:left-0 hover:before:w-full'>
-                                <span className='relative z-10 flex gap-4 '>
+                            <button className="text-red hover:before:bg-redborder-red-500 relative overflow-hidden bord er border-red-500 rounded-md bg-white px-6 py-4 text-red-500 shadow-2xl transition-all before:absolute before:bottom-0 before:left-0 before:top-0 before:z-0 before:h-full before:w-0 before:bg-red-500 before:transition-all before:duration-500 hover:text-white hover:shadow-red-500 hover:before:left-0 hover:before:w-full">
+                                <span className="relative z-10 flex gap-4 ">
                                     Shop Now
                                     <svg
-                                        xmlns='http://www.w3.org/2000/svg'
-                                        fill='none'
-                                        viewBox='0 0 24 24'
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        fill="none"
+                                        viewBox="0 0 24 24"
                                         strokeWidth={1.5}
-                                        stroke='currentColor'
-                                        className='w-6 h-6'
+                                        stroke="currentColor"
+                                        className="w-6 h-6"
                                     >
-                                        <path strokeLinecap='round' strokeLinejoin='round' d='M17.25 8.25 21 12m0 0-3.75 3.75M21 12H3' />
+                                        <path
+                                            strokeLinecap="round"
+                                            strokeLinejoin="round"
+                                            d="M17.25 8.25 21 12m0 0-3.75 3.75M21 12H3"
+                                        />
                                     </svg>
                                 </span>
                             </button>
@@ -44,25 +51,27 @@ function Home() {
                     </div>
                 </div>
                 {/* Khung 2 hình nhỏ */}
-                <div className='flex relative flex-col justify-center items-start w-full h-full gap-3'>
-                    <div className='flex relative flex-col justify-center items-start px-20 h-full w-full'>
-                        <img src='/2.jpg' alt='Banner1' className='absolute -z-0 left-0 top-0 w-full h-full' />
-                        <div className='z-10 flex flex-col gap-3'>
-                            <h2 className='text-5xl text-[#ff4f4f] cursor-hover'>
-                                45% <span className='discount text-base cursor-hover '>OFF</span>
+                <div className="hidden lg:flex relative flex-col justify-center items-start w-full h-full gap-3">
+                    <div className="flex relative flex-col justify-center items-start px-20 h-full w-full">
+                        <img src="/2.jpg" alt="Banner1" className="absolute -z-0 left-0 top-0 w-full h-full" />
+                        <div className="z-10 flex flex-col gap-3">
+                            <h2 className="text-5xl text-[#ff4f4f] cursor-hover">
+                                45% <span className="discount text-base cursor-hover ">OFF</span>
                             </h2>
-                            <h3 className='text-[28px] text-[#0da487] font-extrabold cursor-hover'>Nut Collection</h3>
+                            <h3 className="text-[28px] text-[#0da487] font-extrabold cursor-hover">Nut Collection</h3>
                         </div>
                     </div>
-                    <div className='flex relative flex-col justify-center items-start px-20 h-full w-full'>
-                        <img src='/3.jpg' alt='Banner1' className='absolute -z-0 left-0 top-0 w-full h-full' />
+                    <div className="flex relative flex-col justify-center items-start px-20 h-full w-full">
+                        <img src="/3.jpg" alt="Banner1" className="absolute -z-0 left-0 top-0 w-full h-full" />
                     </div>
                 </div>
             </div>
             {/* Khu slider thứ 2 */}
-            <div className='2xl:container 2xl:mx-auto 2xl:px-0 py-3 px-10'>
+            <div className="h-full w-full m-8">
                 <MyCarousel />
             </div>
+            {/* khu 3 */}
+            <div className="flex flex-col h-[500px] w-full bg-red-500 z-20">121312312323</div>
         </div>
     );
 }
