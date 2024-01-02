e53d6b3f730d007d72af1d46a065f6306ffb9160
SA DAM <dams@dolphinsolutions.vn>
2024-01-02 11:34:46 +0700

    update layout

--- fe-FreshFood/src/pages/Products/index.jsx
+++ fe-FreshFood/src/pages/Products/index.jsx
@@ -25,32 +25,28 @@ function Products() {
     }
 
     return (
-        <div className="flex flex-col w-full h-auto p-8 sm:px-12 lg:px-20 xl:px-28 overflow-y-scroll overflow-x-hidden items-center justify-start">
+        <div className='flex flex-col w-full h-auto p-8 sm:px-12 lg:px-20 xl:px-28 overflow-y-scroll overflow-x-hidden items-center justify-start'>
             {/* khu Sản phẩm */}
-            <div className="w-full h-full grid grid-cols-1 sm:grid-cols-3 lg:grid-cols-4">
+            <div className='w-full h-full grid grid-cols-1 sm:grid-cols-3 lg:grid-cols-4'>
                 {/* khu Category */}
-                <div className="flex flex-col jite gap-2 bg-slate-100 rounded-2xl p-1 lg:p-2 mr-1 sm:mr-2 lg:mr-3 xl:mr-4 2xl:mr-5">
-                    <div className="flex flex-col w-full items-start justify-center gap-1">
-                        <span className="text-3xl font-semibold select-none">Category</span>
-                        <hr className=" bg-green-400 w-12 h-1 border-0 rounded" />
+                <div className='flex flex-col jite gap-2 bg-slate-100 rounded-2xl p-1 lg:p-2 mr-1 sm:mr-2 lg:mr-3 xl:mr-4 2xl:mr-5'>
+                    <div className='flex flex-col w-full items-start justify-center gap-1'>
+                        <span className='text-3xl font-semibold select-none'>Category</span>
+                        <hr className=' bg-green-400 w-12 h-1 border-0 rounded' />
                     </div>
                     <HoverLinks />
                 </div>
                 {/* khu Category */}
                 {products ? (
-                    <div className="flex flex-col gap-8 sm:col-span-2 lg:col-span-3">
-                        <div className="flex flex-col">
-                            <div className="flex flex-col gap-1 w-full items-end justify-center">
-                                <span className="text-neutral-900 text-3xl font-semibold select-none">
-                                    Tất cả sản phẩm
-                                </span>
-                                <hr className=" bg-green-400 w-48 h-1 border-0 rounded" />
+                    <div className='flex flex-col gap-8 sm:col-span-2 lg:col-span-3'>
+                        <div className='flex flex-col'>
+                            <div className='flex flex-col gap-1 w-full items-end justify-center'>
+                                <span className='text-neutral-900 text-3xl font-semibold select-none'>Tất cả sản phẩm</span>
+                                <hr className=' bg-green-400 w-48 h-1 border-0 rounded' />
 
-                                <span className="text-[#4a5568]">
-                                    Lựa chọn sản phẩm tươi ngon với giá cả phải chăng.
-                                </span>
+                                <span className='text-[#4a5568]'>Lựa chọn sản phẩm tươi ngon với giá cả phải chăng.</span>
                             </div>
-                            <div className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 gap-3 p-1">
+                            <div className='grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 gap-3 p-1'>
                                 {products.map((product, index) => {
                                     return <CardProduct key={index} product={product} />;
                                 })}
@@ -58,7 +54,7 @@ function Products() {
                         </div>
                     </div>
                 ) : (
-                    <div className="flex items-center justify-center">Loading...</div>
+                    <div className='flex items-center justify-center'>Loading...</div>
                 )}
             </div>
         </div>
