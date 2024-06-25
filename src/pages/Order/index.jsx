12b58802622952bbde47f9fa34f211be550aea40
Bùi Tống Minh Châu <buitongminhchau01032002@gmail.com>
2024-06-25 08:11:36 +0700

    update

--- src/pages/Order/index.jsx
+++ src/pages/Order/index.jsx
@@ -432,7 +432,7 @@ export default function Order() {
                                     {[...vouchers]?.map((_voucher) => (
                                         <div
                                             className={clsx(
-                                                'mb-2 mr-2 w-64 cursor-pointer rounded border-2 px-4 py-2',
+                                                'mb-2 mr-2 w-72 cursor-pointer rounded border-2 px-4 py-2',
                                                 {
                                                     'border-green-600':
                                                         voucher?.id === _voucher?.id,
